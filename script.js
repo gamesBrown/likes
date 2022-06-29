5bc44bffb4602e61ee875f71f72b852ae48bcaeb
gamesBrown <studio13productions@outlook.com>
2022-06-29 09:48:32 -0700

    cool likes

---
+++ script.js
@@ -0,0 +1,30 @@
+function incrementLike1()
+{
+    
+    var input = document.querySelector('#button1_Count');
+    input.innerText++;
+
+    
+    
+
+}
+function incrementLike2()
+{
+    
+    var input = document.querySelector('#button2_Count');
+    input.innerText++;
+
+    
+    
+
+}
+function incrementLike3()
+{
+    
+    var input = document.querySelector('#button3_Count');
+    input.innerText++;
+
+    
+    
+
+}
